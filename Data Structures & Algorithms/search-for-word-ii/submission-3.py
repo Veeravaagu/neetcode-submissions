14aab7808853faf6e0d93d9a5c45100b630b970e
Veeravaagu <93789055+Veeravaagu@users.noreply.github.com>
2026-04-03 10:54:43 -0400

    Update: search-for-word-ii - submission-3

--- Data Structures & Algorithms/search-for-word-ii/submission-3.py
+++ Data Structures & Algorithms/search-for-word-ii/submission-3.py
@@ -1,50 +1,42 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+        ROWS, COLS = len(board), len(board[0])
+        res, visit = set(), set()
+
+        def dfs(r, c, root, word):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] not in root.children or (r, c) in visit):
+                return
+            visit.add((r, c))
+            root = root.children[board[r][c]]
+            word += board[r][c]
+            if root.isWord:
+                res.add(word)
+
+            dfs(r + 1, c, root, word)  
+            dfs(r - 1, c, root, word) 
+            dfs(r, c + 1, root, word) 
+            dfs(r, c - 1, root, word)
+
+            visit.remove((r, c))
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+        return list(res)           
+
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.endWord = False
+        self.isWord = False
+    def addWord(self, words):
+        curr = self
+        for c in words:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.isWord = True
 
-class Solution:
-    def __init__(self):
-        self.root = TrieNode()
-        
-    def addWord(self,  words: List[str]):
-        for word in words:
-            curr = self.root
-            for c in word:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-            curr.endWord = True
-
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        self.addWord(words)
-        lengthofrow = len(board)
-        lengthofcolumn = len(board[0])
-        # I Had the idea to move +1 and -1 between Rows and Columns and solved until here I also had the idea to use index mapping to make sure the same node is not visited again like (0,0) or (0,1) or (1,0).
-        res, visit = set(), set()
-        def dfs(r: int, c: int, node: TrieNode, word: str) -> None:
-            if (r < 0 or c < 0 or r >= lengthofrow or
-                c >= lengthofcolumn or (r,c) in visit or
-                board[r][c] not in node.children):
-
-                return
-            visit.add((r, c))
-            node = node.children[board[r][c]]
-            word += board[r][c]
-            if node.endWord:
-                res.add(word)
-            dfs(r + 1, c, node, word)
-            dfs(r - 1, c, node, word)
-            dfs(r, c + 1, node, word)
-            dfs(r, c - 1, node, word)
-            visit.remove((r, c))
-
-        for r in range(lengthofrow):
-            for c in range(lengthofcolumn):
-                dfs(r, c, self.root, "")
-        return list(res)
-
-
-
-
+            
         
